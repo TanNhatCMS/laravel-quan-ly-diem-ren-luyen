f08f9528ca528433c286766b7b5c6e1312b8cdcb
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-02-08 23:36:36 +0700

    chore clear code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -81,8 +81,9 @@ class AuthController extends BaseController
      */
     public function logout()
     {
+        $user = auth()->user()->token();
+        $user->revoke();
         auth()->logout();
-
         return $this->sendResponse([], 'Successfully logged out.');
     }
 
@@ -113,56 +114,46 @@ class AuthController extends BaseController
         ];
     }
 
-    public function changePassword(PasswordChangeRequest $request): JsonResponse
-    {
-        $user = auth()->user();
-        if ($user && Hash::check($request->old_password, $user->password)) {
-            User::find($user->id)
-                ->update([
-                    'password' => Hash::make($request->password),
-                ]);
-
-            return $this->successResponse([
-                'message' => 'Password has been changed',
-            ]);
-        }
-
-        return $this->failedResponse();
-    }
-
-    public function updateProfile(ProfileUpdateRequest $request): JsonResponse
-    {
-        $user = auth()->user();
-        // check unique email except this user
-        if (isset($request->email)) {
-            $check = User::where('email', $request->email)
-                ->where('id', '!=', $user->id)
-                ->first();
-
-            if ($check) {
-                return $this->failedResponse('The email address is already used!');
-            }
-        }
-
-        $user->update(
-            $request->only([
-                'name',
-                'email',
-            ])
-        );
-
-        return $this->successResponse([
-            'message' => 'Profile updated successfully!',
-        ]);
-    }
-
-    public function logout(Request $request): JsonResponse
-    {
-        $user = auth()->user()->token();
-        $user->revoke();
-
-        return $this->successResponse([
-            'message' => 'Logged out succesfully!',
-        ]);
-    }
+//    public function changePassword(PasswordChangeRequest $request): JsonResponse
+//    {
+//        $user = auth()->user();
+//        if ($user && Hash::check($request->old_password, $user->password)) {
+//            User::find($user->id)
+//                ->update([
+//                    'password' => Hash::make($request->password),
+//                ]);
+//
+//            return $this->successResponse([
+//                'message' => 'Password has been changed',
+//            ]);
+//        }
+//
+//        return $this->failedResponse();
+//    }
+//
+//    public function updateProfile(ProfileUpdateRequest $request): JsonResponse
+//    {
+//        $user = auth()->user();
+//        // check unique email except this user
+//        if (isset($request->email)) {
+//            $check = User::where('email', $request->email)
+//                ->where('id', '!=', $user->id)
+//                ->first();
+//
+//            if ($check) {
+//                return $this->failedResponse('The email address is already used!');
+//            }
+//        }
+//
+//        $user->update(
+//            $request->only([
+//                'name',
+//                'email',
+//            ])
+//        );
+//
+//        return $this->successResponse([
+//            'message' => 'Profile updated successfully!',
+//        ]);
+//    }
 }
