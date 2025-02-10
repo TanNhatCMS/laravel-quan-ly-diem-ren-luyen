3ec6b4395118b9fb16ef20d0319ceb4cad055eac
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-02-11 02:46:54 +0700

    chore fix PermissionManager

--- database/seeders/PermissionManagerTablesSeeder.php
+++ database/seeders/PermissionManagerTablesSeeder.php
@@ -2,21 +2,25 @@
 
 namespace Database\Seeders;
 
-use Backpack\PermissionManager\app\Models\Permission;
-use Backpack\PermissionManager\app\Models\Role;
+use App\Models\Permission;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 
 class PermissionManagerTablesSeeder extends Seeder
 {
-    protected $roles = [
-        'superadmin',
-        'admin',
-        'student',
+    protected array $roles = [
+        'SuperAdmin', // 1
+        'Admin', // 2
+        'StudentAffairsOffice',// 3. phòng công tác sinh viên
+        'AcademicAdvisor', //4. cố vấn học tập
+        'Lecturer', //5. giảng viên
+        'ClassOfficer', //6. ban cán sự lớp
+        'Student', // sinh viên
     ];
 
-    protected $permissionsRoles = [
+    protected array $permissionsRoles = [
         'manage news' => [1, 2],
         'manage pages' => [1, 2],
         'manage menu items' => [1, 2],
@@ -24,8 +28,23 @@ class PermissionManagerTablesSeeder extends Seeder
         'manage roles' => [1],
         'manage permissions' => [1],
         'file manager' => [1, 2],
-        'logs' => [1],
+        'Logs' => [1],
         'backups' => [1],
+
+        'Browse student' => [1, 2, 3, 4, 5, 6],
+        'Create student' => [1, 2],
+        'Update student' => [1, 2],
+        'Delete student' => [1, 2],
+
+        'Browse lecturer' => [1, 2],
+        'Create lecturer' => [1, 2],
+        'Update lecturer' => [1, 2],
+        'Delete lecturer' => [1, 2],
+
+        'manage academic advisor' => [1, 2],
+        'manage student affairs office' => [1, 2],
+        'manage course' => [1, 2],
+        'manage class' => [1, 2],
     ];
 
     /**
@@ -33,7 +52,7 @@ class PermissionManagerTablesSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
@@ -45,11 +64,11 @@ class PermissionManagerTablesSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         foreach ($this->roles as $role) {
-            Role::create(['name' => $role, 'guard_name' => 'backpack']);
+            Role::create(['name' => $role, 'guard_name' => 'web']);
         }
 
         foreach ($this->permissionsRoles as $permission => $roles) {
-            Permission::create(['name' => $permission, 'guard_name' => 'backpack'])->roles()->sync($roles);
+            Permission::create(['name' => $permission, 'guard_name' => 'web'])->roles()->sync($roles);
         }
 
         // Super admin on first user
